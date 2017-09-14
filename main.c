5383c60996b35b9c9d9f65c20a8aba5822b1ac3e
Gianni Georg Weinand <gi4nn1@yahoo.de>
2017-09-14 01:37:37 -0300

    Funcionalidad basica
    Falta modularizar, documentar, hacer que funcione por consola.

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-#define WORDSIZE 30
+#define WORDLENGTH 30
 
 typedef unsigned int uns;
 
@@ -24,7 +25,7 @@ typedef struct _Bucket{
 
 typedef struct _BucketList{
 	Bucket **arr;
-	uns sz;
+	uns sz;	
 } BucketList;
 
 typedef int (*BucketPointerCompare) (Bucket* , Bucket*);
@@ -41,7 +42,7 @@ Bucket* bucket_crear(){
 }
 
 Bucket* bucket_insertar(Bucket *b, char *key){
-	char *w = (char*) malloc(sizeof(char) * WORDSIZE);
+	char *w = (char*) malloc(sizeof(char) * WORDLENGTH);
 	strcpy(w, key);
 	BucketNode* new = (BucketNode*) malloc(sizeof(BucketNode));
 	new->w = w;
@@ -52,6 +53,14 @@ Bucket* bucket_insertar(Bucket *b, char *key){
 	return b;
 }
 
+void bucket_destruir(Bucket *b){
+	BucketNode *act, *sig;
+	for(act = b->list; act != NULL; act = sig){
+		sig = act->next;
+		free(act);
+	}
+}
+
 BucketList* bucketlist_crear(uns sz){
 	BucketList *BL = (BucketList*) malloc(sizeof(BucketList));
 	BL->arr = (Bucket**) malloc(sz * sizeof(Bucket*));
@@ -60,14 +69,18 @@ BucketList* bucketlist_crear(uns sz){
 }
 
 void bucketlist_destruir(BucketList* BL, int eliminaElementos){
+	if(eliminaElementos){
+		int i;
+		for(i = 0; i < BL->sz; ++i)
+			bucket_destruir(BL->arr[i]);
+	}
 	free(BL->arr);
 	free(BL);
-	if(eliminaElementos);
-		//Completar
 }
 
-//Ordena de mayor a menor
+//Ordena de mayor a menor con merge sort.
 BucketList* bucketlist_sort(BucketList *BL, BucketPointerCompare comp){
+	//Caso base.
 	if(BL->sz < 2)
 		return BL;
 		
@@ -83,18 +96,13 @@ BucketList* bucketlist_sort(BucketList *BL, BucketPointerCompare comp){
 	for(i = m; i < BL->sz; ++i)
 		A2->arr[i - m] = BL->arr[i];
 	
-	//printf("pude crear los chiquitos con size %i y %i\n", A1->sz, A2->sz);
 		
 	A1 = bucketlist_sort(A1, comp);
 	A2 = bucketlist_sort(A2, comp);
 	
-	//printf("pude ordenar los chiquitos\n");
-	
 	i = j = k = 0;
 	//i en A1, j en A2, k en BL
 	for(k=0; k < BL->sz ; ++k){
-		//printf("voy a meter el elemento %i\n", k);
-		
 		//A1 esta vacio
 		if(i >= A1->sz){
 			//printf("A1 vacio\n");
@@ -109,35 +117,21 @@ BucketList* bucketlist_sort(BucketList *BL, BucketPointerCompare comp){
 			continue;
 		}
 		
-		//printf("voy a comparar A1 en el indice %i A2 en el indice %i\n", i, j);
 		//Si el primer bucket no asignado en A2 es menor al de A1
-		if(comp(A2->arr[j], A1->arr[i])){
-			//printf("dio true\n");
+		if(comp(A2->arr[j], A1->arr[i]))
 			//Copiamos el de A1
 			BL->arr[k] = A1->arr[i++];
-		}
-		else{
-			//printf("dio false\n");
+		
+		else
 			//En caso contrario copiamos el de A2
 			BL->arr[k] = A2->arr[j++];
-		}
-		//printf("meti el elemento %i\n", k);
+			
 	}
-	
-	//printf("pude mergear\n");
 	
 	bucketlist_destruir(A1, 0);
 	bucketlist_destruir(A2, 0);
 	
 	return BL;
-}
-
-void bucket_destruir(Bucket *b){
-	BucketNode *act, *sig;
-	for(act = b->list; act != NULL; act = sig){
-		sig = act->next;
-		free(act);
-	}
 }
 
 //FNV-1a de 32 bits
@@ -224,7 +218,7 @@ PerfectHash* ph_crear(char **D, uns dicSize){
 		
 		G[hash(0, wordList[0]->w) % dicSize] = d;
 		for(j = 0; j < actual->sz; ++j){
-			TH[ slots[j] ] = (char*) malloc(sizeof(char) * WORDSIZE);
+			TH[ slots[j] ] = (char*) malloc(sizeof(char) * WORDLENGTH);
 			strcpy(TH[ slots[j] ], wordList[j]->w);
 		}
 		
@@ -251,9 +245,11 @@ PerfectHash* ph_crear(char **D, uns dicSize){
 		int lugar = libres[libresIdx++];
 		//Restamos 1 para asegurarnos que queda negativo
 		G[hash(0, word) % dicSize] = -lugar-1;
-		TH[lugar] = (char*) malloc(sizeof(char) * WORDSIZE);
+		TH[lugar] = (char*) malloc(sizeof(char) * WORDLENGTH);
 		strcpy(TH[lugar], word);
 	}
+	
+	bucketlist_destruir(BL, 1);
 	
 	ph->HT = TH;
 	
@@ -284,42 +280,158 @@ int ph_buscar(PerfectHash *ph, char *key){
 		return 0;
 }
 
-int main(){
-	FILE *lemario;
-	lemario = fopen("lemario.txt", "r");
-	uns dicSize=0; //( ͡° ͜ʖ ͡°)
+//Intercambia idx con el siguiente, es decir, idx+1
+void swap_ady(char *s, int idx){
+	char aux = s[idx];
+	s[idx] = s[idx+1];
+	s[idx+1] = aux;
+}
+
+//Borra el n-esimo caracter del string
+void del_char(char *s, int n){
+	int i, idx=0;
+	int l = strlen(s);
+	for(i = 0; i < l; ++i)
+		if(i != n)
+			s[idx++] = s[i];
+			
+	s[idx] = '\0';
+}
+
+//Agrega el caracter c en la posición idx de la cadena s, desplazando los caracteres existentes a la derecha. 
+void add_char(char *s, int idx, char c){
 	int i;
-	char w[WORDSIZE];
+	int l = strlen(s);
+	char ant = c;
+	char sig = c;
 	
-	while(fgets(w, WORDSIZE, lemario) != NULL)
+	for(i=idx; i <= l; ++i){
+		ant = sig;
+		sig = s[i];
+		s[i] = ant;
+	}
+	
+	s[i] = '\0';
+}
+
+//s1 = [0, idx) s2 = [idx, l)
+void str_split(char *s, int idx, char *s1, char *s2){
+	int i;
+	int l = strlen(s);
+	s1 = strncpy(s1, s, idx);
+	s1[idx] = '\0';
+	
+	for(i = idx; i < l; ++i)
+		s2[i - idx] = s[i];
+		
+	s2[i-idx] = '\0';
+}
+
+int main(){
+	FILE *lemario, *texto, *output;
+	lemario = fopen("lemario.txt", "r");
+	texto = fopen("texto.txt", "r");
+	output = fopen("output.txt", "w");
+	uns dicSize=0; //( ͡° ͜ʖ ͡°)
+	int i,j;
+	char w[WORDLENGTH];
+	
+	while(fgets(w, WORDLENGTH, lemario) != NULL)
 		++dicSize;
 	
 	rewind(lemario);
 	char **D = (char**) malloc(sizeof(char*) * dicSize);
-	for(i = 0; fgets(w, WORDSIZE, lemario) != NULL; ++i) {
+	for(i = 0; fgets(w, WORDLENGTH, lemario) != NULL; ++i) {
 		//Eliminamos el \n
 		w[strlen(w) - 1] = '\0';
 		//Reservamos lugar y guardamos la palabra leida
-		D[i] = (char*) malloc(sizeof(char) * WORDSIZE);
+		D[i] = (char*) malloc(sizeof(char) * WORDLENGTH);
 		strcpy(D[i], w);
 	}
 		
 	PerfectHash *ph = ph_crear(D, dicSize);
 	
-	rewind(lemario);
-	while(fgets(w, WORDSIZE, lemario) != NULL){
-		w[strlen(w) - 1] = '\0';
-		if(ph_buscar(ph, w) == 0)
-			printf("%s\n", w);
+	
+	int notEOF = 1;
+	while(notEOF){
+		char c = 'a';
+		for(i = 0; ;){
+			if(fscanf(texto, "%c", &c) == EOF){
+				notEOF = 0;
+				break;
+			}
+			
+			if(ispunct(c) || c == ' ' || c == '\n')
+				break;
+			w[i++] = tolower(c);
+		}
+		w[i] = '\0';
+		if(strlen(w) && (!ph_buscar(ph, w))){
+			fprintf(output, "La siguiente palabra es incorrecta: %s\n", w);
+			fprintf(output, "Sugerencias:\n");
+			
+			char *dummy = malloc(sizeof(char) * WORDLENGTH);
+			strcpy(dummy, w);
+			int l = strlen(w);
+			char *minus = "abcdefghijklmnopqrstuvwxyz";
+			int m = strlen(minus);
+			
+			//Reemplazar una letra por otra
+			for(i = 0; i < l; ++i){
+				strcpy(dummy, w);
+				for(j = 0; j < m; ++j){
+					dummy[i] = minus[j];
+					if(ph_buscar(ph, dummy))
+						fprintf(output, "%s\n", dummy);
+				}
+			}
+			
+			//Intercambiar un par de caracteres adyacentes
+			for(i = 0; i < l-1; ++i){
+				strcpy(dummy, w);
+				swap_ady(dummy, i);
+				if(ph_buscar(ph, dummy))
+					fprintf(output, "%s\n", dummy);
+			}
+			
+			//Eliminar un caracter de la palabra
+			for(i = 0; i < l; ++i){
+				strcpy(dummy, w);
+				del_char(dummy, i);
+				if(ph_buscar(ph, dummy))
+					fprintf(output, "%s\n", dummy);
+			}
+			
+			//Insertar un caracter entre un par de caracteres
+			for(i = 0; i < l; ++i){
+				for(j = 0; j < m; ++j){
+					strcpy(dummy, w);
+					add_char(dummy, i, minus[j]);
+					if(ph_buscar(ph, dummy))
+						fprintf(output, "%s\n", dummy);
+				}
+			}
+			
+			
+			//Separar la palabra en dos
+			char *s1 = (char*) malloc(sizeof(char) * WORDLENGTH);
+			char *s2 = (char*) malloc(sizeof(char) * WORDLENGTH);
+			for(i = 0; i < l; ++i){
+				strcpy(dummy, w);
+				str_split(dummy, i, s1, s2);
+				if(ph_buscar(ph, s1) && ph_buscar(ph, s2))
+					fprintf(output, "%s %s\n", s1, s2);
+			}
+			
+			fprintf(output, "\n");
+		}	
 	}
 	
-	while(scanf("%s", w)){
-		if(ph_buscar(ph, w) == 0)
-			printf("%s\n", w);
-	}
 	
+	for(i = 0; i < dicSize; ++i)
+		free(D[i]);
+	free(D);
 	fclose(lemario);
-	//No hace destruir el diccionario pues es parte de ph
 	ph_destruir(ph);
 	
 	return 0;
